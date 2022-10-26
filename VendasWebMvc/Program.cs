490e05ac210aa0adbc65f45a2410019bce21ee4d
Raphael Teixeira <raphaelsoares11@hotmail.com>
2022-10-26 01:25:38 -0300

    Criando CRUD com Scaffolding

--- VendasWebMvc/Program.cs
+++ VendasWebMvc/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VendasWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<VendasWebMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("VendasWebMvcContext") ?? throw new InvalidOperationException("Connection string 'VendasWebMvcContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
